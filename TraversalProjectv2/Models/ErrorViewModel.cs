679e76eaee820451ccc971d8f9b65dda791a9741
EmreTnc1 <emrtnc871@gmail.com>
2023-06-23 15:33:29 +0300

    Proje dosyası ekle.

---
+++ TraversalProjectv2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TraversalProjectv2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
