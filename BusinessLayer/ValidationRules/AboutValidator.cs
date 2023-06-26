5de72e416528b9a7cd8193201594feed15b360d2
faruk selman demirci <96984158+FarukSelmanDemirci@users.noreply.github.com>
2023-06-26 20:43:30 +0300

    Üyelik ve Rol Sistemlerinin Eklendi

--- BusinessLayer/ValidationRules/AboutValidator.cs
+++ BusinessLayer/ValidationRules/AboutValidator.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BusinessLayer.ValidationRules
+﻿namespace Business.ValidationRules
 {
-    internal class AboutValidator
+    public class AboutValidator : AbstractValidator<About>
     {
+        public AboutValidator()
+        {
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Description cannot be empty ...!");
+            RuleFor(x => x.Image1).NotEmpty().WithMessage("Please select an image");
+            RuleFor(x => x.Description).MinimumLength(50).WithMessage("Write a description of at least 50 characters");
+            RuleFor(x => x.Description).MaximumLength(1500).WithMessage("Please shorten the description");
+        }
     }
+
+  
 }
