6b31d3ef3ac49137b04c5878010020914dc43fcc
EmreTnc1 <emrtnc871@gmail.com>
2023-07-01 16:01:13 +0300

    34 - Platform Ayarları ve Görünüm Bileşeni Eklenmesi 37 - Admin Crud 38 - Admin Layout Partial Views

---
+++ TraversalProjectv2/Areas/Admin/Views/Destination/AddDestination.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TraversalProjectv2.Areas.Admin.Views.Destination
+{
+    public class AddDestinationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
