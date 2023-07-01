0fa9f7e96e802a411103a77f6108ad389937b9aa
Selman <selman4352@gmail.com>
2023-07-01 15:01:00 +0300

    24 - Kullanıcı Profil Eklenmesi 25 - Profil Güncelleme ve Profil Foto Ekleme 27 - Rezervasyon Sayfasının Eklenmesi 29 - Rezervasyon Onaylama 30- Reservation ve Destination 33 - Rehber Listesinin Eklenmesi

---
+++ DataAccessLayer/EntityFramework/EfReservationDal.cs
@@ -0,0 +1,31 @@
+﻿using DataAccess.Abstract;
+using DataAccess.Concrete;
+using DataAccess.Repository;
+using Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.EntityFramework
+{
+    public class EfReservationDal : GenericRepository<Reservation>, IReservationDal
+    {
+        public List<Reservation> GetListWithReservationByApproved(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Reservation> GetListWithReservationByPrevious(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Reservation> GetListWithReservationByWaitApproval(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- EntityLayer/Concrete/Reservation.cs
+++ EntityLayer/Concrete/Reservation.cs
@@ -1,5 +1,4 @@
-﻿using EntityLayer.Concrete;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,16 +6,17 @@ using System.Threading.Tasks;
 
 namespace Entity.Concrete
 {
-	public class Reservation
-	{
-		public int ReservationID { get; set; }
-		public int AppUserId { get; set; }
-		public AppUser AppUser { get; set; }
-		public string PersonCount { get; set; }
-		public string Destination { get; set; }
-		public DateTime ReservationDate { get; set; }
-		public string Description { get; set; }
-		public string Status { get; set; }
+    public class Reservation
+    {
+        public int ReservationID { get; set; }
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+        public string PersonCount { get; set; }
+        public DateTime ReservationDate { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public int DestinationId { get; set; }
+        public Destination Destination { get; set; }
 
-	}
+    }
 }
